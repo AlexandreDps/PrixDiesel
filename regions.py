e9a55f3af7f5ace703af3a515f26e5cb28162203
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-05 23:00:52 +0200

    Update regions.py

--- regions.py
+++ regions.py
@@ -7,30 +7,20 @@ import folium
 #la j'affecte à la variable m une carte centrée sur la france
 m = folium.Map(location=[46.232192999999995, 2.209666999999996], zoom_start=5)
 
-#là je trace le contour de la région aquitaine
-aquitaine = f"https://polygons.openstreetmap.fr/get_geojson.py?id=8637&params=0"
-folium.GeoJson(aquitaine, name="geojson").add_to(m)
+#liste des identifiants OSM de chaque région
+l = ["8637", "3792883", "8654", "3792877", "3792878", "3792876", "4217435", "3793170"]
 
-#là c'est l'occitanie avec comme couleur le veeeeeeeeeerttttttttt
-# color pour bordure et fillColor pour interieeeuurrr
+for identifiant in l:
+    region = f"https://polygons.openstreetmap.fr/get_geojson.py?id={identifiant}&params=0"
+    folium.GeoJson(region, name="geojson").add_to(m)
+
+'''
+exemple changement couleur : utile pour après
+
+là c'est l'occitanie avec comme couleur le veeeeeeeeeerttttttttt
+color pour bordure et fillColor pour interieeeuurrr
+
 style = {'fillColor': '#228B22', 'color': '#228B22'}
 herault = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3792883&params=0"
 folium.GeoJson(herault, name="geojson", style_function=lambda x:style).add_to(m)
-
-paca = f"https://polygons.openstreetmap.fr/get_geojson.py?id=8654&params=0"
-folium.GeoJson(paca, name="geojson").add_to(m)
-
-ara = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3792877&params=0"
-folium.GeoJson(ara, name="geojson").add_to(m)
-
-bfc = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3792878&params=0"
-folium.GeoJson(bfc, name="geojson").add_to(m)
-
-ge = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3792876&params=0"
-folium.GeoJson(ge, name="geojson").add_to(m)
-
-hdf = f"https://polygons.openstreetmap.fr/get_geojson.py?id=4217435&params=0"
-folium.GeoJson(hdf, name="geojson").add_to(m)
-
-norm = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3793170&params=0"
-folium.GeoJson(norm, name="geojson").add_to(m)
+'''
