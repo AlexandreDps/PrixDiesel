a956d3762e225588208e4b2836f3abd0623d295e
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-05 18:45:04 +0200

    Update regions.py

--- regions.py
+++ regions.py
@@ -16,3 +16,21 @@ folium.GeoJson(aquitaine, name="geojson").add_to(m)
 style = {'fillColor': '#228B22', 'color': '#228B22'}
 herault = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3792883&params=0"
 folium.GeoJson(herault, name="geojson", style_function=lambda x:style).add_to(m)
+
+paca = f"https://polygons.openstreetmap.fr/get_geojson.py?id=8654&params=0"
+folium.GeoJson(paca, name="geojson").add_to(m)
+
+ara = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3792877&params=0"
+folium.GeoJson(ara, name="geojson").add_to(m)
+
+bfc = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3792878&params=0"
+folium.GeoJson(bfc, name="geojson").add_to(m)
+
+ge = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3792876&params=0"
+folium.GeoJson(ge, name="geojson").add_to(m)
+
+hdf = f"https://polygons.openstreetmap.fr/get_geojson.py?id=4217435&params=0"
+folium.GeoJson(hdf, name="geojson").add_to(m)
+
+norm = f"https://polygons.openstreetmap.fr/get_geojson.py?id=3793170&params=0"
+folium.GeoJson(norm, name="geojson").add_to(m)
