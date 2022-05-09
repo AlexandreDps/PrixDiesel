432e4e327d60df494795a7ca18e01d1f589672f1
Alemanu211 <90306622+Alemanu211@users.noreply.github.com>
2022-05-09 18:46:18 +0200

    Add files via upload

---
+++ Variations.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  9 18:20:24 2022
+
+@author: descompa
+"""
+
+import DownloadData as DD
+import DataSorting as DS
+
+
+#Mise à jour de la data
+DD.get_ten_Mins_Data()
+DD.get_dailyData()
+
+
+#Dictionnaires prix (Instantané)
+prixParDepartements = DS.DictDepartements("PrixCarburants_instantane.xml")
+prixParCantons = DS.DictCantons("PrixCarburants_instantane.xml")
+
+
+
+chaine = DS.get_chaine()
+#Dictionnaires prix (Instantané)
+prixParDepartementsB = DS.DictDepartements("PrixCarburants_quotidien_" + chaine + ".xml")
+prixParCantonsB = DS.DictCantons("PrixCarburants_quotidien_" + chaine + ".xml")
+
+
+
